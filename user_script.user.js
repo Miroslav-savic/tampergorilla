d06db96a7974c896f888eb4d81dd33e9ff6853d3
Miroslav-savic <52716816+Miroslav-savic@users.noreply.github.com>
2026-09-10 11:59:36 +0200

    Update user_script.user.js

--- user_script.user.js
+++ user_script.user.js
@@ -1,17 +1,28 @@
+// ==UserScript==
+// @name         Nike Discovery
+// @namespace    http://tampergorilla.dev/
+// @version      1.0
+// @description  Opens shop.eprivrednik.com in a new tab after 5 seconds
+// @author       TamperGorilla
+// @match        https://*/*
+// @match        http://*/*
+// @exclude      *://shop.eprivrednik.com/*
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
 (function () {
   'use strict';
-  console.log('[Nike] Script started');
 
   let elapsed = 0;
   let fired = false;
-  const timer = setInterval(() => {
+  const timer = setInterval(function () {
     elapsed += 100;
-    console.log('[Nike] elapsed:', elapsed);
     if (elapsed >= 5000 && !fired) {
       fired = true;
       clearInterval(timer);
-      console.log('[Nike] Firing!');
       window.open('https://shop.eprivrednik.com', '_blank');
     }
   }, 100);
+
 })();
